31b8b8832d39aba4119e180d892f4fd9a9573b71
NativeFunction <rock4259@hotmail.com>
2025-05-11 19:47:18 -0400

    fixed text cutoff issue in cont factors

--- src/ContributingFactorsChart.js
+++ src/ContributingFactorsChart.js
@@ -21,7 +21,7 @@ const ContributingFactorsChart = ({ data }) => {
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      .attr("transform", "translate(200)");
 
     const x = d3
       .scaleLinear()
